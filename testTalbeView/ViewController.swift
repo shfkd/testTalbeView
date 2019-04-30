8871b1850004d16a06ef580f476e57ca015ba065
D7703_07 <d7703_07@lulzsec.local>
2019-04-30 15:20:37 +0900

    Initial Commit

---
+++ testTalbeView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTalbeView
+//
+//  Created by D7703_07 on 2019. 4. 30..
+//  Copyright © 2019년 1234. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
